708c2cf19982353f8e014db4973ffe1e04c1f129
Voliwey <125397102+Voliwey@users.noreply.github.com>
2023-07-30 21:03:13 +0300

    Ops

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,9 @@
 const form = document.querySelector(".login-form")
-// console.log(form);
 
 const formSubmit = (event) => {
      event.preventDefault();
 
     const formElements = event.currentTarget.elements;
-    // console.log(formElements);
     if (formElements.email.value === "" || formElements.password.value === "") {
         return alert("Вам потрібно заповнити всі поля!")
     }
